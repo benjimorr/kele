2049cc7c603c0b9ae7ac446c20aa6b8bbf7d32c5
Ben Morrison <benmorrison0@gmail.com>
2017-10-10 18:38:51 -0400

    Checkpoint 3 Retrieve Users: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,6 @@
 require 'HTTParty'
 require 'pp'
+require 'json'
 
 class Kele
     include HTTParty
@@ -8,18 +9,11 @@ class Kele
         @email = email
         @password = password
         @base_uri = 'https://www.bloc.io/api/v1'
-        @user_auth
+        @user_auth = self.class.post(@base_uri + '/sessions', body: { "email": @email, "password": @password })["auth_token"]
     end
 
-    def get_auth
-        options = {
-            body: {
-                "email": @email,
-                "password": @password
-            }
-        }
-
-        response = self.class.post(@base_uri + '/sessions', options)
-        @user_auth = response["auth_token"]
+    def get_me
+        response = self.class.get(@base_uri + '/users/me', headers: { "authorization" => @user_auth })
+        JSON.parse(response.body)
     end
 end
