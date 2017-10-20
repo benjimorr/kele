21688ca08483514af266f90f6ea6d87a27dc516a
Ben Morrison <benmorrison0@gmail.com>
2017-10-19 22:01:56 -0400

    Checkpoint 7 submission: original attempt

--- lib/kele.rb
+++ lib/kele.rb
@@ -18,11 +18,24 @@ class Kele
 
     def get_me
         response = self.class.get(@base_uri + '/users/me', headers: { "authorization" => @user_auth })
+        @enrollment_id = response["current_enrollment"]["id"]
         JSON.parse(response.body)
     end
 
     def get_mentor_availability(mentor_id)
         response = self.class.get(@base_uri + "/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @user_auth })
         JSON.parse(response.body)
+    end
+
+    def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+        return "Error: enrollment_id is not defined. Use the #get_me method." unless defined? @enrollment_id
+        response = self.class.post(@base_uri + '/checkpoint_submissions', headers: { "authorization" => @user_auth },
+            body: {
+                "assignment_branch": assignment_branch,
+                "assignment_commit_link": assignment_commit_link,
+                "checkpoint_id": checkpoint_id,
+                "comment": comment,
+                "enrollment_id": @enrollment_id
+            })
     end
 end
